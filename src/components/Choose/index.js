28cbe6fc78865aa80d6c41ac2009e8b116eac035
Jihan <parkjihan5253@gmail.com>
2023-03-27 13:32:40 +0900

    1

--- src/components/Choose/index.js
+++ src/components/Choose/index.js
@@ -6,12 +6,15 @@ const Choose = ({money, setMoney, bet, setBet, phase, setPhase, inform, setInfor
     const [deck, setDeck] = useState([[10,"spade"],[5,"heart"],[11,"diamond"],[8,"clover"]]);
 
     const cardShuffle = () => {
+        setPhase(2);
+        setInform("Shuffling...");
         let shuffling = [[1,"spade"],[2,"spade"],[3,"spade"],[4,"spade"],[5,"spade"],[6,"spade"],[7,"spade"],[8,"spade"],[9,"spade"],[10,"spade"],[11,"spade"],[12,"spade"],[13,"spade"],[1,"diamond"],[2,"diamond"],[3,"diamond"],[4,"diamond"],[5,"diamond"],[6,"diamond"],[7,"diamond"],[8,"diamond"],[9,"diamond"],[10,"diamond"],[11,"diamond"],[12,"diamond"],[13,"diamond"],[1,"heart"],[2,"heart"],[3,"heart"],[4,"heart"],[5,"heart"],[6,"heart"],[7,"heart"],[8,"heart"],[9,"heart"],[10,"heart"],[11,"heart"],[12,"heart"],[13,"heart"],[1,"clover"],[2,"clover"],[3,"clover"],[4,"clover"],[5,"clover"],[6,"clover"],[7,"clover"],[8,"clover"],[9,"clover"],[10,"clover"],[11,"clover"],[12,"clover"],[13,"clover"]];
         for(let i = shuffling.length - 1; i > 0; i--){
             const j = Math.floor(Math.random() * (i + 1));
             [shuffling[i], shuffling[j]] = [shuffling[j], shuffling[i]];
         }
         setDeck((prev) => {return[...shuffling]});
+        setTimeout(function(){setPhase(1);setInform("액션을 선택해주세요.");},1000);
     }
 
     const cardDraw = (deck, setDeck, setHand) => {
@@ -33,13 +36,7 @@ const Choose = ({money, setMoney, bet, setBet, phase, setPhase, inform, setInfor
         setBetting(10);
         getScore(setScoreD,deckD);
         getScore(setScoreP,deckP);
-        Promise.resolve()
-            .then(cardShuffle)
-            .then(function(){
-                setPhase(1);
-                setInform("액션을 선택해주세요.");
-                setTimeout(function(){console.log(deck)},3000);
-            })
+        cardShuffle();
     }
 
     const getScore = (setScore, deck) => {
